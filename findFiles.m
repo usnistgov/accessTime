be707d952536552f9cb14b876efc6d3070604602
Jaden <jaden.pieper@nist.gov>
2019-02-07 15:33:26 -0700

    Rewrote so that all tossed files are marked in single array

--- findFiles.m
+++ findFiles.m
@@ -7,58 +7,55 @@ function filenames = findFiles(log,local_searchpath,network_searchpath)
 % network search path are copied over to the local search path. 
 
 log.searchPath = local_searchpath;
-filenames = log.datafilenames()';
+loc_names = log.datafilenames()';
 
-% Drop any error sessions that were found
-errSessions = cellfun(@(x) strcmp(':Error',x),filenames);
-filenames = filenames(~errSessions);
+% Identify all sessions identified locally that were marked error
+loc_errSessions = cellfun(@(x) strcmp(':Error',x),loc_names);
 
-notFound = cellfun(@isempty, filenames);
+log.searchPath = network_searchpath;
+net_names = log.datafilenames()';
+% Idenfity all sessions on network marked error
+net_errSessions = cellfun(@(x) strcmp(':Error',x),net_names);
 
-if(any(notFound))
-    % Find it on the network and copy
-    log.searchPath = network_searchpath;
-    filenames_net = log.datafilenames()';
-    errSessions = cellfun(@(x) strcmp(':Error',x),filenames_net);
-        if(any(errSessions))
-            warning([num2str(sum(errSessions)) ' files had errors'])
-            filenames_net = filenames_net(~errSessions);
-            filenames = filenames(~errSessions);
-            notFound = notFound(~errSessions);
-        end
-    notFound_net = cellfun(@isempty,filenames_net);
-    if(all(notFound == notFound_net))
-        warning([num2str(sum(notFound_net)) ' files not found on network'])
-        filenames = filenames(~notFound);
-    else
-        filenames_net = filenames_net(~notFound_net);
-        notFound = notFound(~notFound_net);
-        
-        % Some files on network that aren't local => copy them
-        filenames_parts = cellfun(@(x) strsplit(x,'\'),filenames_net,'UniformOutput',0);
-        % Reorganize into single cell array
-        fileparts = vertcat(filenames_parts{:});
-        
-        for k = 1:size(fileparts,1)
-            if(notFound(k))
-                netpath = filenames_net{k};
-                localpath = fullfile('..',fileparts{k,end-1}, fileparts{k,end});
-                fprintf('Copying from:\n --%s\n', netpath);
-                fprintf('Copying to:\n --%s\n', localpath);
-                
-                [status,msg,msgID] = copyfile(netpath,localpath);
-                if(~status)
-                    error([msg '. ' msgID])
-                end
-            end
-        end
-        
-        %     filenames = log2filenames(log(idx),local_searchpath)';
-        log.searchPath = local_searchpath;
-        filenames = log.datafilenames();
-    end
-elseif(isempty(filenames))
-    error('Could not find any grant files that satisfy parameters')
+% Identify all sessions that could not be identified on network
+net_notFound = cellfun(@isempty, net_names);
+if(any(net_notFound))
+    warning([num2str(sum(net_notFound)) ' files not found on network']);
 end
 
+% Total error sessions are either local or net
+tossSessions = loc_errSessions | net_errSessions | net_notFound;
+
+% Toss errSessions
+loc_names = loc_names(~tossSessions);
+net_names = net_names(~tossSessions);
+
+% Find remaining files missing locally
+loc_notFound = cellfun(@isempty, loc_names);
+
+if(any(loc_notFound))
+    % Some files on network that aren't local => copy them
+    filenames_parts = cellfun(@(x) strsplit(x,'\'),net_names,'UniformOutput',0);
+    % Reorganize into single cell array
+    fileparts = vertcat(filenames_parts{:});
+    
+    for k = 1:size(fileparts,1)
+        if(loc_notFound(k))
+            netpath = net_names{k};
+            
+            localpath = fullfile('..', fileparts{k,end-1}, fileparts{k,end});
+            fprintf('Copying from:\n --%s\n', netpath);
+            fprintf('Copying to:\n --%s\n', localpath);
+            
+            [status,msg,msgID] = copyfile(netpath,localpath);
+            if(~status)
+                error([msg '. ' msgID])
+            end
+        end
+    end
+end
+
+log.searchPath = local_searchpath;
+filenames = log.datafilenames()';
+filenames = filenames(~tossSessions);
 end
