6ca6c2a2da011e5c9fff16aa7c7e4f4563656dbd
Jesse Frey <jesse.frey@NIST.GOV>
2019-05-06 11:12:35 -0600

    Python test script explicit about test
    working away on issue #12

--- ls_test.py
+++ ls_test.py
@@ -1,13 +1,18 @@
 from log_search import log_search
+
+def testSearch(ls,test):
+	print(f"searching with '{repr(test)}'")
+	print(ls.MfSearch(test))
+
 	
 ls=log_search('\\\\cfs2w.nist.gov\\671\\Projects\\MCV\\Access-Time\\')
 
 print(ls.log[-2])
 #run a search
-ls.MfSearch({'operation':'Test','System':' Direct'})
+testSearch(ls,{'operation':'Test','System':' Direct'})
 #clear found
 ls.clear()
 #run another search
-print(ls.MfSearch({'operation':'Test','Rx Device':' 3506'}))
+testSearch(ls,{'operation':'Test','Rx Device':' 3506'})
 #find files
-print(ls.datafilenames())
+print(f"datafilenames returned : {ls.datafilenames()}")
