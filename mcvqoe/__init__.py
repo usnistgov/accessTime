d349271bfa3b4c02ed0cd10e6189e1bb30f83736
Jaden <jaden.pieper@nist.gov>
2021-06-22 16:19:33 -0600

    Added namespace package line

--- mcvqoe/__init__.py
+++ mcvqoe/__init__.py
@@ -5,3 +5,8 @@ from .sliding_delay import sliding_delay_estimates
 from .misc import *
 from .write_log import pre,post
 from .soft_timecode import softTimeDecode
+
+# This line came from here https://packaging.python.org/guides/packaging-namespace-packages/
+# If we have any issues with things that should be imported from this __init__ not working
+# look back to this...
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
