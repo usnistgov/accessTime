fe06ff189f95c9bfc8cc9575a58f7a00b9ad9c2b
Viktor Murray <viktro78@gmail.com>
2021-06-23 11:58:09 -0400

    Use channel plugins in ITS_delay tests

--- tests/test_ITS_delay.py
+++ tests/test_ITS_delay.py
@@ -1,144 +1,304 @@
 import io
 import pkgutil
 import unittest
-from math import exp
 
 import mcvqoe
 import numpy as np
 import scipy
 from mcvqoe.ITS_delay_est import active_speech_level
 
-audio_files = ["test.wav", "test_nb_12200.wav", "test_wb_23850.wav"]
-expected_speech_level = [-29.079965170524318, -31.12585881587833, -30.115341804098797]
+try:
+    # try to import importlib.metadata
+    from importlib.metadata import entry_points
+except ModuleNotFoundError:
+    # fall back to importlib_metadata
+    from importlib_metadata import entry_points
+
+channels = entry_points()["mcvqoe.channel"]
 
 
-def load_audio_file(name):
-    audio_file = pkgutil.get_data("mcvqoe", "audio_clips/" + name)
-    audio_file = io.BytesIO(audio_file)
-    return scipy.io.wavfile.read(audio_file)
+class Sim:
+    def __init__(self):
+        self.channel_rate = None
+        self.sample_rate = 16000
+        self.channel_impairment = None
+        self.print_args = False
+
+
+sim = Sim()
+
+
+def audio_channel_with_overplay(audio_fs, audio_data, chan):
+    audio_with_overplay = np.concatenate(
+        (audio_data, np.zeros([audio_fs], dtype=np.int16))
+    )
+    chan_out = chan.simulate_audio_channel(sim, audio_with_overplay)
+    chan_out = chan_out[: len(audio_with_overplay)]
+    return chan_out
 
 
 class ITSTest(unittest.TestCase):
+    def assert_tol(self, x, y, tol=0, msg=None):
+        self.assertGreaterEqual(x, y - tol, msg)
+        self.assertLessEqual(x, y + tol, msg)
+
+    def verify_variable_delay(
+        self, res, delay_expected, delay_pos, tol=(2000, 100), msg=None
+    ):
+        for pos, dly in zip(*res):
+            if pos > delay_pos + tol[0]:
+                self.assert_tol(dly, delay_expected[1], tol=tol[1], msg=msg)
+            elif pos < delay_pos - tol[0]:
+                self.assert_tol(dly, delay_expected[0], tol=tol[1], msg=msg)
+
     def test_basic_no_delay(self):
-        for name in audio_files:
-            audio_fs, audio_data = load_audio_file(name)
-            audio_data = np.concatenate((np.zeros([1], dtype=np.int16), audio_data))
+        base_file = pkgutil.get_data("mcvqoe", "audio_clips/test.wav")
+        base_file = io.BytesIO(base_file)
+        base_fs, base_data = scipy.io.wavfile.read(base_file)
 
-            expected_length = len(audio_data)
+        for chan in channels:
+            chan = chan.load()
+            chan_out = audio_channel_with_overplay(base_fs, base_data, chan)
+
+            expected_length = len(base_data) + base_fs
             expected_length = expected_length - (expected_length % 2) - 2
+            expected_delay = int(chan.standard_delay * base_fs)
 
-            self.assertEqual(
-                mcvqoe.ITS_delay_est(audio_data, audio_data, "f", fs=audio_fs),
-                (expected_length, 0),
-                msg=name,
-            )
+            delay_calc = mcvqoe.ITS_delay_est(base_data, chan_out, "f", fs=base_fs)
 
-            self.assertEqual(
-                mcvqoe.ITS_delay_est(audio_data, audio_data, "v", fs=audio_fs),
-                ((expected_length,), (0,)),
-                msg=name,
-            )
-
-            self.assertEqual(
-                mcvqoe.ITS_delay_est(audio_data, audio_data, "u", fs=audio_fs),
-                ((expected_length,), (0,)),
-                msg=name,
-            )
+            self.assert_tol(delay_calc[0], expected_length, msg=chan.__name__)
+            self.assert_tol(delay_calc[1], expected_delay, msg=chan.__name__)
 
     def test_basic_fixed_delay(self):
-        for name in audio_files:
-            audio_fs, audio_data = load_audio_file(name)
+        base_file = pkgutil.get_data("mcvqoe", "audio_clips/test.wav")
+        base_file = io.BytesIO(base_file)
+        base_fs, base_data = scipy.io.wavfile.read(base_file)
 
-            audio_fixed_delay = np.concatenate(
-                (np.zeros([20], dtype=np.int16), audio_data)
-            )
-            expected_length = len(audio_data)
-            expected_length = expected_length - (expected_length % 2) + 18
-            self.assertEqual(
-                mcvqoe.ITS_delay_est(audio_data, audio_fixed_delay, "f", fs=audio_fs),
-                (expected_length, 20),
-                msg=name,
-            )
+        for chan in channels:
+            chan = chan.load()
 
-            expected_length = len(audio_data) - 5
-            expected_length = expected_length - (expected_length % 2) - 2
-            self.assertEqual(
-                mcvqoe.ITS_delay_est(audio_data, audio_data[5:], "f", fs=audio_fs),
-                (expected_length, -6 + (len(audio_data) % 2 * 2)),
-                msg=name,
-            )
+            for dly in [10, 100, 1000]:
+                audio_fixed_delay = np.concatenate(
+                    (np.zeros([dly], dtype=np.int16), base_data)
+                )
+                chan_out = audio_channel_with_overplay(base_fs, audio_fixed_delay, chan)
+
+                expected_length = len(audio_fixed_delay) + base_fs
+                expected_length = expected_length - (expected_length % 2) - 2
+
+                expected_delay = int(chan.standard_delay * base_fs) + dly
+
+                delay_calc = mcvqoe.ITS_delay_est(base_data, chan_out, "f", fs=base_fs)
+
+                self.assert_tol(
+                    delay_calc[0],
+                    expected_length,
+                    msg=f"{chan.__name__}, delay: {dly} samples",
+                )
+                self.assert_tol(
+                    delay_calc[1],
+                    expected_delay,
+                    msg=f"{chan.__name__}, delay: {dly} samples",
+                )
+
+                audio_fixed_delay = base_data[dly:]
+
+                chan_out = audio_channel_with_overplay(base_fs, audio_fixed_delay, chan)
+
+                expected_length = len(audio_fixed_delay) + base_fs
+                expected_length = expected_length - (expected_length % 2) - 2
+                expected_delay = int(chan.standard_delay * base_fs) - dly
+
+                delay_calc = mcvqoe.ITS_delay_est(base_data, chan_out, "f", fs=base_fs)
+                self.assert_tol(
+                    delay_calc[0],
+                    expected_length,
+                    msg=f"{chan.__name__}, delay: {dly} samples",
+                )
+                self.assert_tol(
+                    delay_calc[1],
+                    expected_delay,
+                    msg=f"{chan.__name__}, delay: {dly} samples",
+                )
 
     def test_basic_variable_delay(self):
-        for name in audio_files:
-            audio_fs, audio_data = load_audio_file(name)
+        base_file = pkgutil.get_data("mcvqoe", "audio_clips/test.wav")
+        base_file = io.BytesIO(base_file)
+        base_fs, base_data = scipy.io.wavfile.read(base_file)
 
-            audio_var_delay = np.concatenate(
-                (
-                    audio_data[5:10000],
-                    np.zeros([100], dtype=np.int16),
-                    audio_data[10000:],
+        for chan in channels:
+            chan = chan.load()
+
+            for dly in [(19200, 1000), (53040, 2000), (113000, 3000)]:
+                audio_var_delay = np.concatenate(
+                    (
+                        base_data[: dly[0]],
+                        np.zeros([dly[1]], dtype=np.int16),
+                        base_data[dly[0] :],
+                    )
                 )
-            )
-            expected_length = len(audio_var_delay)
-            expected_length = expected_length - (expected_length % 2) - 2
-            self.assertEqual(
-                mcvqoe.ITS_delay_est(audio_data, audio_var_delay, "v", fs=audio_fs),
-                (
-                    (8368, expected_length),
-                    (-6 + (len(audio_data) % 2 * 2), 94 + (len(audio_data) % 2 * 2)),
-                ),
-                msg=name,
-            )
+                chan_out = audio_channel_with_overplay(base_fs, audio_var_delay, chan)
+                delay_calc = mcvqoe.ITS_delay_est(base_data, chan_out, "v", fs=base_fs)
+
+                try:
+                    self.verify_variable_delay(
+                        delay_calc,
+                        (
+                            int(chan.standard_delay * base_fs),
+                            int(chan.standard_delay * base_fs) + dly[1],
+                        ),
+                        dly[0],
+                        msg=f"{chan.__name__}, delay: {dly[1]} samples at position {dly[0]}",
+                    )
+                except AssertionError as e:
+                    print(delay_calc)
+                    raise e
+
+                audio_var_delay = np.concatenate(
+                    (base_data[: dly[0]], base_data[dly[0] + dly[1] :])
+                )
+                chan_out = audio_channel_with_overplay(base_fs, audio_var_delay, chan)
+                delay_calc = mcvqoe.ITS_delay_est(base_data, chan_out, "v", fs=base_fs)
+
+                try:
+                    self.verify_variable_delay(
+                        delay_calc,
+                        (
+                            int(chan.standard_delay * base_fs),
+                            int(chan.standard_delay * base_fs) - dly[1],
+                        ),
+                        dly[0],
+                        msg=f"{chan.__name__}, delay: {-dly[1]} samples at position {dly[0]}",
+                    )
+                except AssertionError as e:
+                    print(delay_calc)
+                    raise e
 
     def test_basic_unknown_delay(self):
-        for name in audio_files:
-            audio_fs, audio_data = load_audio_file(name)
+        base_file = pkgutil.get_data("mcvqoe", "audio_clips/test.wav")
+        base_file = io.BytesIO(base_file)
+        base_fs, base_data = scipy.io.wavfile.read(base_file)
 
-            audio_fixed_delay = np.concatenate(
-                (np.zeros([20], dtype=np.int16), audio_data)
-            )
-            expected_length = len(audio_fixed_delay)
-            expected_length = expected_length - (expected_length % 2) - 2
-            self.assertEqual(
-                mcvqoe.ITS_delay_est(audio_data, audio_fixed_delay, "u", fs=audio_fs),
-                ((expected_length,), (20,)),
-                msg=name,
-            )
+        for chan in channels:
+            chan = chan.load()
 
-            self.assertEqual(
-                mcvqoe.ITS_delay_est(audio_data, audio_fixed_delay, "u", fs=audio_fs),
-                ((expected_length,), (20,)),
-                msg=name,
-            )
-            audio_var_delay = np.concatenate(
-                (
-                    audio_data[5:10000],
-                    np.zeros([100], dtype=np.int16),
-                    audio_data[10000:],
+            for dly in [0, 10, 100, 1000]:
+                audio_fixed_delay = np.concatenate(
+                    (np.zeros([dly], dtype=np.int16), base_data)
                 )
-            )
-            expected_length = len(audio_var_delay)
-            expected_length = expected_length - (expected_length % 2) - 2
-            self.assertEqual(
-                mcvqoe.ITS_delay_est(audio_data, audio_var_delay, "u", fs=audio_fs),
-                (
-                    (8368, expected_length),
-                    (-6 + (len(audio_data) % 2 * 2), 94 + (len(audio_data) % 2 * 2)),
-                ),
-                msg=name,
-            )
+                chan_out = audio_channel_with_overplay(base_fs, audio_fixed_delay, chan)
+
+                expected_length = len(audio_fixed_delay) + base_fs
+                expected_length = expected_length - (expected_length % 2) - 2
+
+                expected_delay = int(chan.standard_delay * base_fs) + dly
+
+                delay_calc = mcvqoe.ITS_delay_est(base_data, chan_out, "u", fs=base_fs)
+
+                self.verify_variable_delay(
+                    delay_calc,
+                    (expected_delay,),
+                    expected_length,
+                    msg=f"{chan.__name__}, delay: {dly} samples",
+                )
+
+                audio_fixed_delay = base_data[dly:]
+
+                chan_out = audio_channel_with_overplay(base_fs, audio_fixed_delay, chan)
+
+                expected_length = len(audio_fixed_delay) + base_fs
+                expected_length = expected_length - (expected_length % 2) - 2
+                expected_delay = int(chan.standard_delay * base_fs) - dly
+
+                delay_calc = mcvqoe.ITS_delay_est(base_data, chan_out, "u", fs=base_fs)
+
+                self.verify_variable_delay(
+                    delay_calc,
+                    (expected_delay,),
+                    expected_length,
+                    msg=f"{chan.__name__}, delay: {-dly} samples",
+                )
+
+            for dly in [(19200, 1000), (53000, 2000), (113000, 3000)]:
+                audio_var_delay = np.concatenate(
+                    (
+                        base_data[: dly[0]],
+                        np.zeros([dly[1]], dtype=np.int16),
+                        base_data[dly[0] :],
+                    )
+                )
+                chan_out = audio_channel_with_overplay(base_fs, audio_var_delay, chan)
+                delay_calc = mcvqoe.ITS_delay_est(base_data, chan_out, "u", fs=base_fs)
+
+                try:
+                    self.verify_variable_delay(
+                        delay_calc,
+                        (
+                            int(chan.standard_delay * base_fs),
+                            int(chan.standard_delay * base_fs) + dly[1],
+                        ),
+                        dly[0],
+                        msg=f"{chan.__name__}, delay: {dly[1]} samples at position {dly[0]}",
+                    )
+                except AssertionError as e:
+                    print(delay_calc)
+                    raise e
+
+                audio_var_delay = np.concatenate(
+                    (base_data[: dly[0]], base_data[dly[0] + dly[1] :])
+                )
+                chan_out = audio_channel_with_overplay(base_fs, audio_var_delay, chan)
+                delay_calc = mcvqoe.ITS_delay_est(base_data, chan_out, "u", fs=base_fs)
+
+                try:
+                    self.verify_variable_delay(
+                        delay_calc,
+                        (
+                            int(chan.standard_delay * base_fs),
+                            int(chan.standard_delay * base_fs) - dly[1],
+                        ),
+                        dly[0],
+                        msg=f"{chan.__name__}, delay: {-dly[1]} samples at position {dly[0]}",
+                    )
+                except AssertionError as e:
+                    print(delay_calc)
+                    raise e
 
     def test_active_speech_level(self):
-        for i, name in enumerate(audio_files):
-            audio_fs, audio_data = load_audio_file(name)
+        base_file = pkgutil.get_data("mcvqoe", "audio_clips/test.wav")
+        base_file = io.BytesIO(base_file)
+        base_fs, base_data = scipy.io.wavfile.read(base_file)
 
-            self.assertLess(
-                abs(
-                    active_speech_level(audio_data, fs=audio_fs)
-                    - expected_speech_level[i]
-                ),
-                1e-4,
-                msg=name,
+        expected_speech_levels = [
+            -29.202315980890447,
+            -31.125858989851054,
+            -30.160492075065143,
+        ]
+        for i, chan in enumerate(channels):
+            chan = chan.load()
+
+            chan_out = audio_channel_with_overplay(base_fs, base_data, chan)
+            self.assert_tol(
+                active_speech_level(chan_out, fs=base_fs),
+                expected_speech_levels[i],
+                tol=1e-6,
+                msg=f"{chan.__name__}, no delay",
+            )
+
+            audio_var_delay = np.concatenate(
+                (
+                    base_data[:100000],
+                    np.zeros([20000], dtype=np.int16),
+                    base_data[10000:],
+                )
+            )
+
+            chan_out = audio_channel_with_overplay(base_fs, audio_var_delay, chan)
+            self.assert_tol(
+                active_speech_level(chan_out, fs=base_fs),
+                expected_speech_levels[i],
+                tol=0.1,
+                msg=f"{chan.__name__}, 20000 samples delay at position 100000",
             )
 
 
