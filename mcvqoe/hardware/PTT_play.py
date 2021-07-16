ae2ea446983e22acdc28ed0b9fe359e398165211
Jesse <jesse.frey@nist.gov>
2021-07-16 07:22:59 -0600

    Fix audio clip load in PTT_play

--- mcvqoe/hardware/PTT_play.py
+++ mcvqoe/hardware/PTT_play.py
@@ -33,7 +33,7 @@ def single_play(ri, ap, audio_file=None, playback=False, ptt_wait=0.68, save_nam
     """
 
     if audio_file is None:
-        audio_file = io.BytesIO(pkgutil.get_data("mcvqoe", "audio_clips/test.wav"))
+        audio_file = io.BytesIO(pkgutil.get_data("mcvqoe.audio_clips", "test.wav"))
 
     # get fs from audio player
     fs = ap.sample_rate
