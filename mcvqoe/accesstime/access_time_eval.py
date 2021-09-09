ca2c16105c9792f886835edf881cd3803bd07516
William Magrogan <william.magrogan@nist.gov>
2021-09-09 17:08:17 -0600

    Get AccessData class working

--- mcvqoe/accesstime/access_time_eval.py
+++ mcvqoe/accesstime/access_time_eval.py
@@ -12,29 +12,82 @@ import warnings
 
 import pandas as pd
 import numpy as np
-from scipy import curve_fit
+from scipy.optimize import curve_fit
 
 import mcvqoe.math
+
 
 # =============================================================================
 # Class definitions
 # =============================================================================
-class evaluate(object):
+class AccessData(object):
     def __init__(self, session_names, cut_names, session_dir, cut_dir):
-        # ToDo: validation logic here as well.
+        self.dat, self.header_dat = self._get_data(session_names, session_dir)
+        self.cut_points = self._get_cut_points(cut_names, cut_dir)
+        self.sampling_frequency = self._get_sampling_frequency()
+        self.audio_clips = self._get_audio_clips()
+        self.speaker_word = self._get_speaker_word()
+        
+    def _get_data(self, session_names, session_dir):
         try:
-            self.dat = [pd.read_csv(session_dir + session, skiprows=3) for session in session_names]
-        except:
-            pass
+            dat = [pd.read_csv(session_dir + session, skiprows=3) for session in session_names]
+            header_dat = [pd.read_csv(session_dir + session, sep='=', nrows=2,
+                                      header=None, usecols=[1]) for session in session_names]
+            header_dat = [hd.transpose() for hd in header_dat]
+            for hd in header_dat:
+                hd.columns=["Wav file", "fs"]
+            return (dat, header_dat)
+        except(FileNotFoundError) as err:
+            print(f"Session files can not be found. {err}")
 
+    def _get_cut_points(self, cut_names, cut_dir):
         try:
-            self.cut_points = [pd.read_csv(cut_dir + cut, skiprows=3) for cut in cut_names]
-        except:
-            pass
-        self.sampling_frequency = None
-        self.audio_clips = None
-        self.speaker_word = None
+            cut_points = [pd.read_csv(cut_dir + cut) for cut in cut_names]
+            return(cut_points)
+        except(FileNotFoundError) as err:
+            print(f"Session files can not be found. {err}")
+    
+    def _get_sampling_frequency(self):
+        if not all([(dat["fs"] == self.header_dat[0].iloc[0]["fs"]).any() for dat in self.header_dat]):
+            raise ValueError("Different sampling frequencies.")
+        else:
+            return self.header_dat[0]["fs"]
+        
+    def _get_audio_clips(self):
+        # cheap, easy etraction of the word from the header data
+        audio_clips = [fname["Wav file"] for fname in self.header_dat]
+        return(audio_clips)        
+        
+    def _get_speaker_word(self):
+        # cheap, easy etraction of the word from the header data
+        speaker_words = [fname.iloc[0]["Wav file"][:-4].split("_")[-1] for fname in self.header_dat]
+        return(speaker_words)
 
+class evaluate(object):
+    def __init__(self, session_names, cut_names, session_dir, cut_dir, test_type):
+        # Data loaded in
+        self.access_data = AccessData(session_names, cut_names, session_dir, cut_dir)
+        self.fit_type = self._get_fit_type(test_type)
+        self.fit_data = None
+        
+        # Data from fit
+        self.fit_data = None
+    
+    def _get_fit_type(self, fit_type):
+        valid_fit_types = ["PTT", "SUT", "COR", "LEG"]
+        if not fit_type in valid_fit_types:
+            raise ValueError(f"Fit type not one of: {valid_fit_types}")
+        return fit_type
+    
+    def fit_data(self):
+        pass
+    
+    def eval_intell(self):
+        pass
+    
+    def eval_access(self):
+        pass
+    
     def eval(self):
         pass
 
@@ -50,28 +103,37 @@ def pretty_print(evaluate):
 # Main definition
 # =============================================================================
 def main():
-    # set up parser
-    parser = argparse.ArgumentParser(descrption=__doc__)
+    # # set up parser
+    # parser = argparse.ArgumentParser(description=__doc__)
 
-    parser.add_argument('session_files', type=str, nargs='+', action='extend',
-                        help='Test names')
-    parser.add_argument('cut_files', type=str, nargs='+', action='extend',
-                        help='Cut files corresponding to session files.')
-    parser.add_argument('-sp', '--session-path', default='', type=str,
-                        help='Path to directory containing the session files, defaults to current directory.')
-    parser.add_argument('-cp', '--cut-path', default='', type=str,
-                        help='Path to directory containing the cut files, defaults to current directory.')
+    # parser.add_argument('session_files', type=str, nargs='+', action='extend',
+    #                     help='Test names')
+    # parser.add_argument('cut_files', type=str, nargs='+', action='extend',
+    #                     help='Cut files corresponding to session files.')
+    # parser.add_argument('-sp', '--session-path', default='', type=str,
+    #                     help='Path to directory containing the session files, defaults to current directory.')
+    # parser.add_argument('-cp', '--cut-path', default='', type=str,
+    #                     help='Path to directory containing the cut files, defaults to current directory.')
+    # parser.add_argument('-tt', '--test-type', default='SUT', type=str,
+    #                     help='Path to directory containing the cut files, defaults to current directory.')
 
-    # Get arguments for use in evaluate class
-    args = parser.parse_args()
-    t = evaluate(args.session_files, args.cut_files,
-                 session_dir=args.sessoin_path, cut_dir=args.cut_path)
+    # # Get arguments for use in evaluate class
+    # args = parser.parse_args()
+    # t = evaluate(args.session_files, args.cut_files,
+    #              args.session_path, args.cut_path,
+    #              args.test_type)
 
-    res = t.eval()
+    # res = t.eval()
 
-    # Pretty print result and return
-    pretty_print(res)
-    return(res)
+    # # Pretty print result and return
+    # pretty_print(res)
+    # return(res)
+
+    eta = AccessData(
+        ['capture_PTT-gate-2500ms_24-Nov-2020_07-24-31_F3_b31_w2_law.csv'],
+        ['F3_b31_w2_law.csv'],
+        'C:/Users/wrm3/Desktop/accesstime_local_data/csv/',
+        'C:/Users/wrm3/Desktop/accesstime_local_data/wav/T_2500ms-varyFilled/')
 
 # =============================================================================
 # Execute if run as main script
