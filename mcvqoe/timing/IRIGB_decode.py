84e2ae7f4ad4f36ab8527fbc1cbdd5ec9d0c4c67
Jesse Frey <jesse.frey@nist.gov>
2021-11-09 11:36:06 -0700

    Update initial kmeans conditions

--- mcvqoe/timing/IRIGB_decode.py
+++ mcvqoe/timing/IRIGB_decode.py
@@ -65,12 +65,10 @@ def IRIGB_decode(tca, fs, tc_tol=0.05):
     env = envelope(tca, 40)
 
     #use kmeans to threshold the envelope into 2 clusters
-    #for the initial guesses of center locations, use: mean +/- stdv
-    envMean = np.mean(env)
-    envStdv = np.std(env)
-    init = np.linspace(envMean - envStdv, envMean + envStdv, 2)
-    #take the cluster indices, the first item returned by kmeans
-    env_th = kmeans2(env, init)[1]
+    #use 0 and max as the starting centroids
+    init = (0, 1)
+    #take the cluster indices, raise an error if we don't have 2 centroids
+    _, env_th = kmeans2(env, init, missing='raise')
 
     #find edges assume that signal starts high so that we see the first real
     edges = np.append([0], 1+np.nonzero(np.diff(env_th))[0])
