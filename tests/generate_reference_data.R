03030f3999211a9fa47f7a81e29c3edc671a30fc
Jaden <jaden.pieper@nist.gov>
2021-11-29 10:54:37 -0700

    Started function to make reference data from R for python tests

---
+++ tests/generate_reference_data.R
@@ -0,0 +1,14 @@
+library(accessTime)
+library(dplyr)
+setwd(dirname(sys.frame(1)$ofile))
+#--------------------[Helper functions]----------------------
+ref_word_csv <- function(sut_fit, name){
+  coefs <- sapply(sut_fit, coef)
+  vcovs <- sapply(sut_fit, vcov)
+  rownames(vcovs) <- c("t0, t0", "lambda, t0", "t0, lambda", "lambda, lambda")
+  res <- rbind(coefs, vcovs)
+  write.csv(res, name, quote=FALSE)
+}
+#--------------------[PTT Gate results]-----------------------
+ref_word_csv(ptt_gate, "PTT-gate-word-fits.csv")
+
