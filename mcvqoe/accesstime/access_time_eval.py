9f8c8b32d2a31ffcbcd27b67489daf3f45a60176
Jaden <jaden.pieper@nist.gov>
2021-12-03 17:03:39 -0700

    Started major reworking of accessdata/evaluate to behave more like other measurement evaluate objects

--- mcvqoe/accesstime/access_time_eval.py
+++ mcvqoe/accesstime/access_time_eval.py
@@ -9,6 +9,7 @@ Created on Wed Sep  8 14:26:30 2021
 # =============================================================================
 import argparse
 import os
+import re
 import warnings
 
 import pandas as pd
@@ -17,24 +18,93 @@ from scipy.optimize import curve_fit
 
 import mcvqoe.math
 
-
+def find_session_csvs(session_id, data_path):
+    data_csvs = os.listdir(data_path)
+    
+    sesh_search = re.compile(f'{session_id}_.+.csv')
+    
+    return list(filter(sesh_search.match, data_csvs))
 # =============================================================================
 # Class definitions
 # =============================================================================
-class AccessData(object):
-    def __init__(self, session_names, cut_names, session_dir, cut_dir):
-        self.dat, self.header_dat = self._get_data(session_names, session_dir)
-        self.cut_points = self._get_cut_points(cut_names, cut_dir)
-        self.sampling_frequency = self._get_sampling_frequency()
-        self.audio_clips = self._get_audio_clips()
-        self.speaker_word = self._get_speaker_word()
+class AccessData:
+    # TODO: I don't love this layout...can we leverage data storage better as a default and resort to explicit paths only when necessary?
+    # TODO: Look deeper into how PSuD does this, I forget...
+    def __init__(self,
+                 test_names,
+                 test_path='',
+                 wav_dirs=[],
+                 ):
+        # self.dat, self.header_dat = self._get_data(test_names, test_path)
+        # self.cut_points = self._get_cut_points(cut_names, cut_dir)
+        # self.sampling_frequency = self._get_sampling_frequency()
+        # self.audio_clips = self._get_audio_clips()
+        # self.speaker_word = self._get_speaker_word()
+        
+        
+        
+        if isinstance(test_names, str):
+            test_names = [test_names]
+        if isinstance(wav_dirs, str):
+            wav_dirs = [wav_dirs]
 
-    def _get_data(self, session_names, session_dir):
+        if not wav_dirs:
+            wav_dirs = (None, ) * len(test_names)
+        
+        # Initialize info arrays
+        self.test_names = []
+        self.test_info = dict()
+        
+        # Loop through all the tests and find files
+        for tn, wd in zip(test_names, wav_dirs):
+            # split name to get path and name
+            # if it's just a name all goes into name
+            dat_path, name = os.path.split(tn)
+            # Split extension, ext is empty if none
+            t_name, ext = os.path.splitext(name)
+            
+            # Check if a path was given to a .csv file
+            if not dat_path and not ext == '.csv':
+                # Generate using test_path
+                dat_path = os.path.join(test_path, 'csv')
+                # Find all csvs that match session id t_name
+                dat_file = find_session_csvs(session_id=t_name,
+                                              data_path=dat_path,
+                                              )
+                cp_path = os.path.join(test_path, 'wav')
+            else:
+                cp_path = os.path.join(os.path.dirname(dat_path), 'wav')
+                dat_file = tn
+            
+            # Check if we were given an explicit wav directory
+            if wd:
+                # Use given path
+                cp_path = wd
+                # get test name from wav path
+                # Normalize path first to remave a possible trailing slash
+                t_name = os.path.basename(os.path.normpath(wd))
+            else:
+                # Otherwise get path to the wav dir
+                
+                # Remove possible R in t_name
+                wt_name = t_name.replace('Rcapture', 'capture')
+                cp_path = os.path.join(cp_path, wt_name)
+            self.test_info[t_name] = {'data_path': dat_path,
+                                     'data_file': dat_file,
+                                     'cp_path': cp_path}
+            self.test_names.append(t_name)
+            self.data, self.cps = self.load_sessions()
+            
+    def load_sessions(self):
+        return None, None
+
+    def _get_data(self, test_names, test_path, wav_dirs):
+        
         try:
             dat = []
             header = []
-            for session in session_names:
-                sesh_name = os.path.join(session_dir, session)
+            for session in test_names:
+                sesh_name = os.path.join(test_path, session)
                 # Grab main data, skipping extra header info
                 dat.append(pd.read_csv(sesh_name, skiprows=3))
                 # Grab extra header info
@@ -45,9 +115,9 @@ class AccessData(object):
                                          usecols=[1]).transpose()
                 header_dat.columns = ['Wav file', 'fs']
                 header.append(header_dat)
-            # dat = [pd.read_csv(session_dir + session, skiprows=3) for session in session_names]
-            # header_dat = [pd.read_csv(session_dir + session, sep='=', nrows=2,
-            #                           header=None, usecols=[1]) for session in session_names]
+            # dat = [pd.read_csv(test_path + session, skiprows=3) for session in test_names]
+            # header_dat = [pd.read_csv(test_path + session, sep='=', nrows=2,
+            #                           header=None, usecols=[1]) for session in test_names]
             # header_dat = [hd.transpose() for hd in header_dat]
             # for hd in header_dat:
             #     hd.columns=["Wav file", "fs"]
@@ -78,13 +148,14 @@ class AccessData(object):
         speaker_words = [fname.iloc[0]["Wav file"][:-4].split("_")[-1] for fname in self.header_dat]
         return(speaker_words)
 
-    def __repr__(self):
-        s = f'''AccessData object:
-            Speaker words: {self.speaker_word}
-            Sampling frequency: {self.sampling_frequency.iloc[0]}'''
-        return s
+    # TODO: Reimplement this
+    # def __repr__(self):
+    #     s = f'''AccessData object:
+    #         Speaker words: {self.speaker_word}
+    #         Sampling frequency: {self.sampling_frequency.iloc[0]}'''
+    #     return s
 
-class FitData(object):
+class FitData:
 
     def __init__(self, I0, t0, lam, covar, curve_dat):
         self.I0 = I0
@@ -93,12 +164,57 @@ class FitData(object):
         self.covar = covar
         self.curve_dat = curve_dat
 
-class evaluate(object):
+class evaluate:
+    """
+    Parameters
+    ----------
+    test_names : str or list
+        Name of test, or list of names of tests.
 
-    def __init__(self, session_names, cut_names, session_dir, cut_dir, test_type, ptt_session_names=None):
+    test_path : str
+        Path where test data is stored. Does not need to be passed if
+        test_names contains full paths to files and wav_dirs is set as
+        well.
+
+    wav_dirs : str or list
+        Paths to directories containing audio for a PSuD test. Must contain
+        cutpoints for audio clips in data files.
+
+    test_type : str
+        DESCRIPTION.
+    ptt_session_names : TYPE, optional
+        DESCRIPTION. The default is None.
+    **kwargs : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+    """
+
+    def __init__(self,
+                 test_names,
+                 cut_names,
+                 test_path,
+                 wav_dirs=[],
+                 # TODO: Come up with better default for test_type
+                 test_type=None,
+                 ptt_session_names=None,
+                 json_data=None,
+                 **kwargs):
+        
+        
+
+        for k, v in kwargs.items():
+            if hasattr(self, k):
+                setattr(self, k, v)
+            else:
+                raise TypeError(f'{k} is not a valid keyword argument')
+        
         # Data loaded in
-        self.sut_access_data = AccessData(session_names, cut_names, session_dir, cut_dir)
-        self.ptt_access_data = None if not ptt_session_names else AccessData(ptt_session_names, cut_names, session_dir, cut_dir)
+        self.data = AccessData(test_names, cut_names, test_path, wav_dirs)
+        # TODO: Come up with clean way to handle correction_data
+        # self.correction_data = None if not ptt_session_names else AccessData(ptt_session_names, cut_names, session_dir, cut_dir)
         self.fit_type = self._get_fit_type(test_type)
         self.fit_data = None
 
@@ -113,15 +229,15 @@ class evaluate(object):
 
     def __repr__(self):
         s = f'''evaluate object:
-            Speaker words: {self.sut_access_data.speaker_word}
-            Sampling frequency: {self.sut_access_data.sampling_frequency.iloc[0]}
+            Speaker words: {self.data.speaker_word}
+            Sampling frequency: {self.data.sampling_frequency.iloc[0]}
             Test type: {self.fit_type}'''
         return s
 
     def fit_curve_data(self):
         # Fit SUT tests
-        recenter = [cp["End"][0]/int(self.sut_access_data.sampling_frequency) for cp in self.sut_access_data.cut_points]
-        fresh_dat = [dat[["PTT_time", "P1_Int"]] for dat in self.sut_access_data.dat]
+        recenter = [cp["End"][0]/int(self.data.sampling_frequency) for cp in self.data.cut_points]
+        fresh_dat = [dat[["PTT_time", "P1_Int"]] for dat in self.data.dat]
         for ii in range(len(recenter)):
             fresh_dat[ii]["PTT_time"] = recenter[ii] - fresh_dat[ii]["PTT_time"]
 
@@ -131,7 +247,7 @@ class evaluate(object):
             curve_dat = pd.concat(fresh_dat)
 
             # Calculate asymptotic int
-            I0 = np.mean([np.mean(x["P2_Int"]) for x in self.sut_access_data.dat])
+            I0 = np.mean([np.mean(x["P2_Int"]) for x in self.data.dat])
 
             # logistic function to fit
             def logistic_fit(xdata, t0, lam):
