8e726b3c6cafacbab95c3998d772bafeb6f8fa75
Jaden <jaden.pieper@nist.gov>
2021-12-03 17:04:12 -0700

    Mapping out ways to initialize access data objects

--- tests/test_accessfits.py
+++ tests/test_accessfits.py
@@ -6,10 +6,9 @@ Created on Mon Nov 29 13:24:11 2021
 """
 import os
 import re
-# import mcvqoe.accesstime as access
-data_path = os.path.join('..', 'accessTime', 'inst', 'extdata')
-csv_path = os.path.join(data_path, 'csv')
-wav_path = os.path.join(data_path, 'wav')
+
+import mcvqoe.accesstime as access
+
 
 
 
@@ -33,10 +32,29 @@ def test_tech(session_name):
             sesh_cut_paths.append(cut_file)
         
     print(f'cut_files: {sesh_cut_paths}\ncsv_files: {sesh_csv_paths}')
-    # access.AccessData(session_names=sesh_csv_paths,
-    #                   cut_names=sesh_cut_paths)
+    ah = access.AccessData(session_names=sesh_csv_paths,
+                      cut_names=sesh_cut_paths,
+                      session_dir='', cut_dir='')
+    import pdb; pdb.set_trace()
 
 if __name__ == '__main__':
+    
+    data_path = os.path.join('..', 'accessTime', 'inst', 'extdata')
+    csv_path = os.path.join(data_path, 'csv')
+    wav_path = os.path.join(data_path, 'wav')
+    
     ptt_session = 'capture_PTT-gate_14-May-2021_07-30-20'
-    test_tech(ptt_session)
+    # test_tech(ptt_session)
+    
+    # Test generic session ID with test_path
+    x1 = access.AccessData(ptt_session, test_path=data_path)
+    
+    # Test specific csv file names with test_path
+    sesh_csvs = access.access_time_eval.find_session_csvs(ptt_session, csv_path)
+    x2 = access.AccessData(test_names=sesh_csvs, test_path=data_path)
+    
+    # TODO:
+    # Test specific csv file names with no test_path
+    
+    # Can you do non-specific wav paths with specific csv paths?
 
