185b956648db595417b81ee3b909be19285d00b5
Jaden Pieper <jaden.pieper@nist.gov>
2021-12-20 11:27:16 -0700

    Add new test files, fix bad .gitignore setup

--- tests/generate_reference_data.R
+++ tests/generate_reference_data.R
@@ -1,3 +1,4 @@
+rm(list=ls())
 library(accessTime)
 library(dplyr)
 setwd(dirname(sys.frame(1)$ofile))
@@ -9,6 +10,25 @@ ref_word_csv <- function(sut_fit, name){
   res <- cbind(t(coefs), t(vcovs))
   write.csv(res, name, quote=FALSE)
 }
-#--------------------[PTT Gate results]-----------------------
-ref_word_csv(ptt_gate, "PTT-gate-word-fits.csv")
 
+rec_access_csv <- function(tech_fits, name){
+  alpha <- seq(from=0.5, to=0.99, by=0.01)
+  access_vals_raW <- lapply(tech_fits,
+               function(x){
+                 eval_access(x, alpha)
+               })
+  access_vals <- bind_rows(access_vals_raW, .id="Technology")
+  write.csv(access_vals, name, quote=FALSE, row.names=FALSE)
+}
+#--------------------[PTT Gate results]-----------------------
+data_path <- file.path("data", "reference_data")
+ref_word_csv(ptt_gate, file.path(data_path, "PTT-gate-word-fits.csv"))
+
+tech_fits <- list(analog_direct=analog_direct,
+                  p25_direct=p25_direct,
+                  p25_trunked_phase1=p25_trunked_phase1,
+                  p25_trunked_phase2=p25_trunked_phase2,
+                  lte=lte
+                  )
+
+rec_access_csv(tech_fits, file.path(data_path, "reference-access-values.csv"))
