d9beae01a5357eb45cc10a8be747350b67a67617
Chelsea Greene <chelsea.greene@NIST.GOV>
2022-01-10 14:51:45 -0700

    Removing flagging, now justa plot placeholder. This is going to split into two other existing scripts

--- mcvqoe/diagnostics/diagnostics_eval.py
+++ mcvqoe/diagnostics/diagnostics_eval.py
@@ -65,31 +65,6 @@ class Diagnostics_Eval():
         self.peak_dbfs = peak_dbfs.to_numpy()
         self.trials = len(diagnostics_dat)      
     
-    def clip_flag(self):
-        """
-        Cycle through the peak amplitude and check
-        for clipping.  
-    
-        Returns
-        -------
-        clip_flag : set
-            Trials that clipped 
-    
-        """
-        # Set up warning threshold
-        vol_high = -1
-        # Create empty set for peak volume and flag
-        clip_flag = set()
-        # Check for positive and negative clipping
-        for n in range(0,self.trials):
-            # If approaching clipping, flag as a bad trial
-            if self.peak_dbfs[n] > vol_high :
-                # Get the name of the wav file
-                clip_wav = self.rx_name[n]
-                # Add it to the bad list
-                clip_flag.add(clip_wav)
-        return clip_flag   
-    
     def fsf_plot(self):
         """
         Plot the FSF of every trial.  
@@ -116,39 +91,6 @@ class Diagnostics_Eval():
         fig.update_xaxes(title_text='Trial Number')
         fig.update_yaxes(title_text='FSF Score')
         fig.show()
-        
-    def fsf_flag(self):
-        """
-        Check for trials with an FSF a certain distance
-        from the mean. Use this info to find trials that 
-        may have lost audio.
-            
-        Returns
-        -------
-        fsf_flag : set
-            Trials that have low FSF scores or otherwise deviate 
-            from the patterns of the dataset
-        """
-        # Create empty set for FSF flag    
-        fsf_flag = set()
-        # Gather metrics for FSF scores
-        fsf_mean = round(statistics.mean(self.fsf_all),3)
-        fsf_std = round(statistics.stdev(self.fsf_all),3)
-        for m in range(0,self.trials):
-            # Cycle through FSF scores, look for trials where the score
-            # stands out by being a certain distance from the mean
-            if abs(self.FSF_all[m]-fsf_mean) > 2*fsf_std:
-                # Get the name of the wav file
-                fsf_flag_wav = self.rx_name[m]
-                # Add it to the bad list
-                fsf_flag.add(fsf_flag_wav)
-            # Add a flag for low FSF scores that may be a sign of 
-            # dropped audio
-            if self.fsf_all[m] < 0.3:
-                # Add it to the bad list
-                fsf_flag.add(fsf_flag_wav)
-                
-        return fsf_flag            
         
     def aw_plot(self):
         """
@@ -177,45 +119,6 @@ class Diagnostics_Eval():
         fig.update_yaxes(title_text='A-Weight (dBA)')
         fig.show()
     
-    def aw_flag(self):
-        """
-        Check for trials with a dBA less than -60 dBA. Check 
-        for trials with a dBA a certain distance from the 
-        mean.   
-    
-        Returns
-        -------     
-        aw_flag : set
-            Trials that have low dBA values (and likely lost audio)
-            or otherwise deviate from the patterns of the dataset 
-    
-        """
-        # Create empty  set for a-weight flag     
-        aw_flag = set()
-        
-        # Calculate mean a-weight, standard deviation.
-        # Use this info to find trials that may have lost
-        # audio. 
-        aw_lin = 10**(self.a_weight/20)
-        aw_low = 10**(-60/20)
-        aw_mean = round(statistics.mean(aw_lin),3)
-        aw_std = round(statistics.stdev(aw_lin),3)
-        for m in range(0,self.trials):
-            # Cycle through AW values, look for trials where the values
-            # stand out by being a certain distance from the mean
-            if abs(aw_lin[m]-aw_mean) > 2*aw_std:
-                # Get the name of the wav file
-                aw_flag_wav = self.rx_name[m]
-                # Add it to the bad list
-                aw_flag.add(aw_flag_wav)
-            # Add a flag if the a-weight is below -60 dBA
-            if aw_lin[m] < aw_low:
-               # Get the name of the wav file
-               aw_flag_wav = self.rx_name[m]
-               # Add it to the bad list
-               aw_flag.add(aw_flag_wav)
-               
-        return aw_flag           
     # TODO fix this for actual use case
     def wav_plot(self,fs):
         """
@@ -250,30 +153,3 @@ class Diagnostics_Eval():
         fig.update_xaxes(title_text='Time (s)')
         fig.update_yaxes(title_text='Amplitude')
         fig.show()  
-     
-    def problem_trials(clip_flag,aw_flag,fsf_flag):     
-        """
-        Warn user of problem trial recording names 
-        
-        Parameters
-        ----------
-        Clip_flag : set    
-            names of trials flagged for clipping
-        AW_flag : set
-            names of trials flagged for their dBA values
-        FSF_flag : set     
-            names of trials flagged for their FSF scores   
-    
-        Returns
-        -------
-    
-        """
-        if clip_flag:
-            np.disp('The following trials were flagged for clipping')
-            np.disp(clip_flag)  
-        if aw_flag:    
-            np.disp('The following trials were flagged for their a-weight')
-            np.disp(aw_flag)
-        if fsf_flag:    
-            np.disp('The following trials were flagged for their FSF scores')
-            np.disp(fsf_flag) 
