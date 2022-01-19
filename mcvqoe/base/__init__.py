a43abf12d7833a8a373ee045d0b6613d698fea16
Jesse <jesse.frey@nist.gov>
2022-01-19 07:16:41 -0700

    Add measurement base class

--- mcvqoe/base/__init__.py
+++ mcvqoe/base/__init__.py
@@ -4,3 +4,4 @@ from .write_log import pre, post
 from .audio_channels import *
 from .fsf import *
 from .naming import get_meas_basename, get_access_clip_info, get_meas_date_str
+from .measurement import Measure
