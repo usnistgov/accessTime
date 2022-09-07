518e1ef9901159c0ed0616ab7c07025c19539f08
Fink <pjf2@NIST.GOV>
2022-09-07 08:41:31 -0600

    Comment change, closes #29

--- mcvqoe/simulation/pbiplugin.py
+++ mcvqoe/simulation/pbiplugin.py
@@ -3,23 +3,23 @@ from .Probabilityiser import PBI
 from .QoEsim import ImpairmentParam
 from numpy import inf
 
-description = 'An impairment to represent a \'flakey\' channel. It is recommended to not use this as a pre impairment.'
+description = 'An impairment to implement an RF-burst mode channel. Recommended to not use this as a pre impairment.'
 
 impairment_type = 'generic'
 
 parameters = {
     'P_a1'     : ImpairmentParam(1.0, float, 'range', min_val=0.0, max_val=1.0,
-                                 interval = 0.001,
-                                 description = 'Original probability transitioning into a transmitting state.'
+                                 interval=0.001,
+                                 description='Original probability transitioning into a transmitting state.'
                                 ),
-    'P_a2'     : ImpairmentParam(1.0, float, 'range', min_val=0.0, max_val=1.0, interval = 0.001,
-                                 description = 'Probability transitioning into a transmitting state.',
+    'P_a2'     : ImpairmentParam(1.0, float, 'range', min_val=0.0, max_val=1.0, interval=0.001,
+                                 description='Probability transitioning into a transmitting state.',
                                 ),
-    'P_r'      : ImpairmentParam(1.0, float, 'range', min_val=0.0, max_val=1.0, interval = 0.001,
-                                 description = 'Probability of remaining in a transmitting state.',
+    'P_r'      : ImpairmentParam(1.0, float, 'range', min_val=0.0, max_val=1.0, interval=0.001,
+                                 description='Probability of remaining in a transmitting state.',
                                 ),
-    'interval' : ImpairmentParam(1.0, float, 'positive', interval = 0.001,
-                                description = 'How often state transitions are evaluated.'
+    'interval' : ImpairmentParam(1.0, float, 'positive', interval=0.001,
+                                description='How often state transitions are evaluated.'
                                 ),
 }
 
